15eccfd3ee47ca5bf526fed2b7d6036487a0d3ca
Anders Norgaard <anders.norgaard@gmail.com>
2011-09-22 23:13:23 +0200

    Add lib-dir

--- getdep.sh
+++ getdep.sh
@@ -1,3 +1,4 @@
 wget http://www.eu.apache.org/dist//commons/net/binaries/commons-net-3.0.1-bin.zip
+mkdir lib
 unzip -j commons-net-3.0.1-bin.zip commons-net-3.0.1/commons-net-3.0.1.jar -d lib
 rm commons-net-3.0.1-bin.zip
